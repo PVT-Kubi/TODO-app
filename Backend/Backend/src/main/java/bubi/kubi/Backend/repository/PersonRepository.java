6109d140c54df9bb880301b470292478fdbe25f1
KubiBubi <81190196+PVT-Kubi@users.noreply.github.com>
2022-12-04 21:45:37 +0100

    Delete Backend directory

--- Backend/Backend/src/main/java/bubi/kubi/Backend/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package bubi.kubi.Backend.repository;
-
-import bubi.kubi.Backend.entity.Person;
-import bubi.kubi.Backend.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-
-}
--- Backend/Backend/src/main/java/bubi/kubi/Backend/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package bubi.kubi.Backend.repository;
-
-import bubi.kubi.Backend.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-
-}
